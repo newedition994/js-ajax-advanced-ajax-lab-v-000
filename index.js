98774eb5b7ab6c1135ef4659e60839d15cf1a96e
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-29 15:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function getRepositories() {
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.sent()
 };
+
  function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
   const src = document.getElementById("repository-template").innerHTML
@@ -11,6 +12,7 @@ function getRepositories() {
   const repoList = template(repos)
   document.getElementById("repositories").innerHTML = repoList
 };
+
  document.addEventListener("DOMContentLoaded", function(event) {
   Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
 });
